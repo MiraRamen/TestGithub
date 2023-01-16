1bf432a3f61fe74d7ec07e2134c96e75fe7dd3bb
MiraRamen <it.sora_st@tni.ac.th>
2023-01-16 15:05:24 +0700

    the first projet by MiraRamen MQTT

---
+++ MQTT_protocol_module.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+#======= Protocol Configuration ======#
+host = 'test.mosquitto.org' # Define the URL  or IP toware the MQTT Broker
+port = 1883                 # Define the specific port toward the MQTT Broker
+group = 'Calculus2'
+yourname = 'Sora'
+topic = group + '/' + yourname # Define the topic to store the data
+username = ''                  # Define the authentication username
+password = ''                  # Define the authentication password
+
+#====== protocol Configuration =====#
+
+client = mqtt.Client()                   # Initialize the connecting gateway to the MQTT server
+client.username_pw_set(username, password)  #Initialize the authentication for the MQTT Broker
+client.connect(host, port)                  # Initialize the MQTT Broker
+
+#==== Protocol Configuraion
+
+data = 55
+print('Input Data Type : {} '.format(type(data)))
+
+#======
+
+try:
+    client.publish(topic = topic, payload = data, qos = 0, retain = True) # Publish the define data into the MQTT Broker
+    print('Publish Success')
+except:
+    print('Publish Fail')    
---
+++ MQTT_subcribe.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+
+#======= Protocol Configuration ======#
+host = 'test.mosquitto.org' # Define the URL  or IP toware the MQTT Broker
+port = 1883                 # Define the specific port toward the MQTT Broker
+group = 'Calculus2'
+yourname = 'Sora'
+topic = group + '/' + yourname # Define the topic to store the data
+username = ''                  # Define the authentication username
+password = ''                  # Define the authentication password
+
+#====== protocol Configuration =====#
+
+client = mqtt.Client()                   # Initialize the connecting gateway to the MQTT server
+client.username_pw_set(username, password)  #Initialize the authentication for the MQTT Broker
+client.connect(host, port)                  # Initialize the MQTT Broker
+
+
+
+#======
+
+try:
+    msg = subscribe.simple(topic, hostname = host, port = port)
+    print('subscribe Success')
+    get_topic = msg.topic
+    data = msg.payload
+    print('{} : {}'.format(get_topic, data))
+    print('Data Type : {}'.format(type(data)))
+   
+except:
+    print('Publish Fail')   
